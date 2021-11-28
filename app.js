c7872bb5e1ec1e5ca76b38fc2fb01a51c5b91ee5
Konstantyn Korenev <korenev@iled.com.ua>
2021-11-28 20:23:57 +0200

    4:11:30

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const person  = {
 	['key_' + (1 + 3)]: 'Computed key',
 	greet() {
 		console.log('greet from person')
+	},
+	info() {
+		console.info('Information about people whose name: ', this.name)
 	}
 }
 
@@ -18,8 +21,79 @@ console.log(person['complex key'])
 console.log(person)
 person.greet()
 
-person.age++
+person.age--
 person.languages.push('PHP')
 delete person['key_4'] // удаляет свойство
 console.log(person)
 
+// const name = person.name
+// const age = person.age
+// const languages = person.languages
+
+// Деструктуризация
+
+const {name, age, languages} = person
+
+console.log(name, age, languages)
+
+const {age: personAge} = person
+
+console.log('Это переопределенное свойство age: ' + personAge)
+
+// Можно установить значение свойства по умолчанию
+
+const {age: personAge2 = 35} = person
+
+console.log('Это переопределенное свойство age: ' + personAge2)
+
+for (let key in person) {
+	if(person.hasOwnProperty(key)) {
+		console.log(key)
+		console.log(person[key])
+	}
+}
+
+/*
+Метод Object.keys() возвращает массив из собственных перечисляемых
+свойств переданного объекта, в том же порядке, в котором они бы обходились
+циклом for...in (разница между циклом и методом в том, что цикл перечисляет
+свойства и из цепочки прототипов).
+*/
+
+/*
+const keys = Object.keys(person)
+keys.forEach((key) => {
+	console.log(key)
+	console.log(person[key])
+})
+*/
+
+Object.keys(person).forEach((key) => {
+	console.log(key)
+	console.log(person[key])
+})
+
+person.info()
+
+const logger = {
+	keys() {
+		console.log('Object Keys: ', Object.keys(this))
+	}
+}
+
+logger.keys()
+
+/*
+Метод bind() создаёт новую функцию, которая при вызове устанавливает в
+качестве контекста выполнения this предоставленное значение. В метод
+также передаётся набор аргументов, которые будут установлены перед
+переданными в привязанную функцию аргументами при её вызове.
+*/
+
+const bound = logger.keys.bind(person)
+
+bound()
+
+// Метод call() вызывает функцию с указанным значением this и индивидуально предоставленными аргументами.
+
+logger.keys.call(person)
