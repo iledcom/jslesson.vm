0393a0ba5bdfd46976ceb859ba81e1109fc516e1
Konstantyn <korenev@iled.com.ua>
2021-12-06 17:05:50 +0200

    5:10:35

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 // DOM
 
-const heding = document.getElementById('hello')
+const heading = document.getElementById('hello')
 
-console.dir(heding)
+// const heading2 = document.getElementsByTagName('h2') - устарел
+// const heading2 = document.getElementsByClassName('h2-class') - устарел
+
+// const heading2 = document.querySelector('h2-class')
+
+const heading2 = document.querySelector('.h2-class')
+
+console.log(heading2)
+
+console.dir(heading.id)
+
+console.dir(heading.textContent)
+
+setTimeout(() => {
+	addStylesTo(heading)
+}, 1500)
+
+function addStylesTo(node) {
+	node.textContent = 'Changed from JavaScript!'
+	node.style.color = 'red'
+	node.style.textAlign = 'center'
+	node.style.backgroundColor = 'blue'
+	node.style.padding = '2rem'
+}
